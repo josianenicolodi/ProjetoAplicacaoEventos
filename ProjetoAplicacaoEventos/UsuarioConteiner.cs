8ff57a0e154d2ac85e5d6c6b1f94bb9ae54c9836
Josiane <Josiane@JOSI>
2016-11-12 21:21:17 -0200

    Iniciada graLerUsuario

---
+++ ProjetoAplicacaoEventos/UsuarioConteiner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+
+
+namespace ProjetoAplicacaoEventos.Usuarios
+{
+
+    
+
+
+    [System.Serializable]
+    class UsuarioConteiner
+    {
+        string path  = @"Usuarios.xml";
+        public List<Usuario> usuarios;
+
+        public void Save()
+        {
+            var serializable = new XmlSerializer(typeof(UsuarioConteiner));
+
+            try
+            {
+                using(var stream = new FileStream(path, FileMode.Create))
+                {
+                    serializable.Serialize(stream, this);
+                    stream.Close();
+                }
+            }
+            catch (Exception e)
+            {
+
+               //Aqui vamos lançar uma exeção
+            }
+        }
+
+        //public static UsuarioConteiner Carrega()
+        //{
+
+        //}
+    }
+}
